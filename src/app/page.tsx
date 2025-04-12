5e94fd8f061afca97dd85f3f90a62d54c2660f70
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2025-04-12 17:15:55 -0500

    finished timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+"use client"
+import { useState } from 'react';
+import { IoIosAdd } from "react-icons/io";
+import { RiSubtractFill } from "react-icons/ri";
 
-export default function Home() {
+export default function Home() {  
+  const [count, setCount] = useState(0);
+
+  function add() {
+    setCount(count + 1);
+  }
+  function subtract() {
+    setCount(count - 1)
+  }
+
   return (
     <>
-      Test
+      <button onClick={subtract}>
+        <RiSubtractFill />
+      </button>
+      { count }
+      <button onClick={add}>
+        <IoIosAdd />
+      </button>
     </>
   );
 }
